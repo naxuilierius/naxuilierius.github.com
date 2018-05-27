503bb9c0e7b44258a923abf5003aa4c3031c8593
NXLR DRAUGIJA <NXLR@NXLR.LT>
2018-05-27 18:36:55 +0300

    OPAPAPAPPAPAPAPA

--- js/main.js
+++ js/main.js
@@ -1,26 +1,10 @@
-// =======================
-var isClicked = false;
-
-function onDoorHover(evt) {
-  console.log('onDoorHover');
-  var audio = document.getElementsByTagName("audio")[0];
-  //playSound(backgroundMusicSource);
-  audio.play();
-}
-
-function onDoorLeave(evt) {
-  console.log('onDoorLeave');
-  var audio = document.getElementsByTagName("audio")[0];
-  if (!isClicked) audio.pause();
-}
-
-function onFirstClick(evt) {
-  console.log('onClick');
-  isClicked = true;
-  var audio = document.getElementsByTagName("audio")[0];
-  audio.play();
-  var firstDiv = document.getElementsByClassName('first')[0];
-  var secondDiv = document.getElementsByClassName('second')[0];
-  firstDiv.className += ' hidden';
-  secondDiv.classList.remove('hidden');
-}
+(function () {
+  var vidElem = document.getElementsByTagName("video")[0];
+  vidElem.addEventListener("play", function () {
+    setTimeout(function () {
+      document.getElementsByClassName("textContainer")[0].className += " show";
+      var audio = document.getElementsByTagName("audio")[0];
+      audio.play();
+    }, 15000);
+  }, false);
+})();
