73a97e7677ade08443c9fdd97e1a97c567d18491
NXLR <Nxlr@nxlr.lt>
2018-06-04 20:37:36 +0300

    OPA VAZHOJAM #!#!@#%

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+var isStarted = false;
+
 (function () {
   var vidElem = document.getElementsByTagName("video")[0];
   vidElem.addEventListener("play", function () {
     setTimeout(function () {
       document.getElementsByClassName("textContainer")[0].className += " show";
-    }, 13500);
-    setTimeout(function () {
-      var audio = document.getElementsByTagName("audio")[0];
-      audio.play();
-    }, 13600);
+    }, 5500);
   }, false);
 })();
+
+function start() {
+  if (isStarted) return;
+  isStarted = true;
+
+  var vidElem = document.getElementsByTagName("video")[0];
+  var audio = document.getElementsByTagName("audio")[0];
+
+  audio.volume = 1;
+  audio.play();
+  vidElem.play();
+}
