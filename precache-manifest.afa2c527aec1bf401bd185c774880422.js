2cadbf425a2b1089596c3b41f4536b950fe28feb
Laurynas Mickonis <mickonis.l@gmail.com>
2019-05-27 22:45:18 +0300

    unwrapped build

---
+++ precache-manifest.afa2c527aec1bf401bd185c774880422.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4684438e719909bd3b7fe0e950fc25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55b5cc77b51bb8b083f8",
+    "url": "/static/css/main.efe9c15d.chunk.css"
+  },
+  {
+    "revision": "ff0c3e8ba660aa6ad679",
+    "url": "/static/js/2.305225de.chunk.js"
+  },
+  {
+    "revision": "55b5cc77b51bb8b083f8",
+    "url": "/static/js/main.956285cd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c9b45c95743952e1fa882efdb86e82d",
+    "url": "/static/media/loader.2c9b45c9.gif"
+  },
+  {
+    "revision": "151c97b211a0f3452e3576d96554dc52",
+    "url": "/static/media/mp4.151c97b2.mp4"
+  }
+]);
